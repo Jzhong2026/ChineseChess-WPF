9b619999ca74b2609f5c9c1ba4c9d86df336d5e6
Jzhong2026 <jason_zhong2022@qq.com>
2026-05-05 14:23:23 +0800

    自定义窗口标题栏与控制按钮

--- ChineseChess/ChineseChess/Views/ShellView.xaml.cs
+++ ChineseChess/ChineseChess/Views/ShellView.xaml.cs
@@ -1,4 +1,5 @@
 using System.Windows;
+using System.Windows.Input;
 
 namespace ChineseChess.Views;
 
@@ -7,5 +8,43 @@ public partial class ShellView : Window
     public ShellView()
     {
         InitializeComponent();
+    }
+
+    private void TitleBar_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ClickCount == 2)
+        {
+            ToggleWindowState();
+            return;
+        }
+
+        if (WindowState == WindowState.Maximized)
+        {
+            WindowState = WindowState.Normal;
+        }
+
+        DragMove();
+    }
+
+    private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+    {
+        WindowState = WindowState.Minimized;
+    }
+
+    private void MaximizeRestoreButton_Click(object sender, RoutedEventArgs e)
+    {
+        ToggleWindowState();
+    }
+
+    private void CloseButton_Click(object sender, RoutedEventArgs e)
+    {
+        Close();
+    }
+
+    private void ToggleWindowState()
+    {
+        WindowState = WindowState == WindowState.Maximized
+            ? WindowState.Normal
+            : WindowState.Maximized;
     }
 }
